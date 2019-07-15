d499bc057c8740bcc7615b43f8017565e28e6344
Koudai <koudai.t822@gmail.com>
2019-07-15 15:08:14 +0900

    third commit

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>こうだい</title>
+    <title>日記</title>
 </head>
 <body>
     <h1><a href=index.php>日記</a></h1>
